375e44b4488352736c869cfa41784bb4c18772cb
merdas369 <3epahvand@gmail.com>
2026-06-12 14:31:01 +0330

    feat: add product model

--- apps/products/models.py
+++ apps/products/models.py
@@ -9,3 +9,15 @@ class Category(TimeStampModel):
     name = models.CharField(max_length=255)
     slug = models.SlugField()
     is_active = models.BooleanField()
+
+
+class Product(TimeStampModel):
+    category = models.ForeignKey(Category, models.PROTECT)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    short_description = models.CharField(max_length=255)
+    description = models.TextField()
+    is_active = models.BooleanField()
+    is_featured = models.BooleanField()
+    meta_title = models.CharField(max_length=255)
+    meta_description = models.TextField()

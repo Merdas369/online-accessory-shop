9fa029fa976b3509a55802530ee09d783dc7635b
merdas369 <3epahvand@gmail.com>
2026-06-12 14:15:06 +0330

    feat: add category model

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+
+from apps.core.models import TimeStampModel
+
 # Create your models here.
+
+
+class Category(TimeStampModel):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    is_active = models.BooleanField()

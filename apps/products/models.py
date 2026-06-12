aab31420bd33abb1e8ca1401771ef9c958e27cb2
merdas369 <3epahvand@gmail.com>
2026-06-12 16:09:23 +0330

    feat: add product variant model

--- apps/products/models.py
+++ apps/products/models.py
@@ -12,7 +12,7 @@ class Category(TimeStampModel):
 
 
 class Product(TimeStampModel):
-    category = models.ForeignKey(Category, models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     short_description = models.CharField(max_length=255)
@@ -21,3 +21,12 @@ class Product(TimeStampModel):
     is_featured = models.BooleanField()
     meta_title = models.CharField(max_length=255)
     meta_description = models.TextField()
+
+
+class ProductVariant(models.Model):
+    product = models.ManyToManyField(Product)
+    material = models.CharField(max_length=255)
+    sku = models.CharField(max_length=255, unique=True)
+    price = models.DecimalField(max_digits=9, decimal_places=0)
+    stock = models.PositiveIntegerField()
+    is_active = models.BooleanField()

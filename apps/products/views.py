b289a53ee3cc946ccdd02ee40c2a6437b1588f3e
merdas369 <3epahvand@gmail.com>
2026-06-18 23:13:47 +0330

    feat: add search field to header

--- apps/products/views.py
+++ apps/products/views.py
@@ -3,10 +3,11 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from .forms import SearchForm
 from .selectors import product_selectors
 
 
 def product_list(request: HttpRequest):
     if request.method == "GET":
         products = product_selectors.get_active_products()
-        return render(request, "products/list.html", {"products": products})
+        return render(request, "products/list.html", {"products": products, "form": SearchForm()})

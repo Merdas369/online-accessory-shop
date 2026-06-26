dbb1c08e626a240c664dbbeb97d0873090c346d2
merdas369 <3epahvand@gmail.com>
2026-06-26 14:12:19 +0330

    feat:add success modal

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from apps.cart.services.cart_service import Cart
+
+
+def cart_function(request: HttpRequest):
+    if request.method == "POST":
+        cart = Cart(request)
+        cart.add_item()
+        return render(request, "components/success_add.html")
+
+
+def add_to_cart(request):
+    return HttpResponse("")
+
+
+def remove_from_cart(request):
+    return HttpResponse("")
+
+
+def update_cart(request):
+    return HttpResponse("")

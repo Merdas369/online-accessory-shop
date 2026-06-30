5c10f20507f6c4530b568af6ae7303d6595c78ef
merdas369 <3epahvand@gmail.com>
2026-06-30 18:11:49 +0330

    feat: add order model

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from apps.core.models import TimeStampModel
+
 
 class OrderStatus(models.TextChoices):
     PENDING = "pending", "Pending"
@@ -9,3 +11,20 @@ class OrderStatus(models.TextChoices):
     CANCELLED = "cancelled", "Cancelled"
     SHIPPED = "shipped", "Shipped"
     DELIVERED = "delivered", "Delivered"
+
+
+class Order(TimeStampModel):
+    uuid = models.UUIDField()
+    status = models.CharField(
+        max_length=20, choices=OrderStatus.choices, default=OrderStatus.PENDING
+    )
+    customer_name = models.CharField(max_length=255)
+    customer_phone = models.CharField(max_length=255)
+    province = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=255)
+    subtotal = models.PositiveIntegerField()
+    vat = models.PositiveIntegerField()
+    shipping_cost = models.PositiveIntegerField()
+    total = models.PositiveIntegerField()

0be4ebf8e140a3daa32814e3a053312f138e7516
merdas369 <3epahvand@gmail.com>
2026-06-30 17:53:01 +0330

    feat: add order status enum

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,1 +1,11 @@
-# Create your models here.
+from django.db import models
+
+
+class OrderStatus(models.TextChoices):
+    PENDING = "pending", "Pending"
+    AWAITING_PAYMENT = "awaiting_payment", "Awaiting_payment"
+    PAID = "paid", "Paid"
+    FAILED = "failed", "Failed"
+    CANCELLED = "cancelled", "Cancelled"
+    SHIPPED = "shipped", "Shipped"
+    DELIVERED = "delivered", "Delivered"
